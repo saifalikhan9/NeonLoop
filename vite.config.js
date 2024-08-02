972357d9c65277030562a022573d010f2dc72312
saifalikhan9 <myname14saif@gmail.com>
2024-08-02 11:04:50 +0530

    chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path"
 export default defineConfig({
   
   plugins: [react()],
+  build: { chunkSizeWarningLimit: 2000, },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
